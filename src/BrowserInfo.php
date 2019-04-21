bcaea53cb2b08dcd5b297ecad0dc1dfa3f28e3cc
Costa <costa365@gmail.com>
2019-04-21 16:54:40 +0300

    Moved BrowserInfo to a class

--- src/BrowserInfo.php
+++ src/BrowserInfo.php
@@ -1,102 +1,120 @@
 <?php
 class BrowserInfo {
-  /*
-  private $uagent = $_SERVER['HTTP_USER_AGENT'];
+  
+  private $uagent = '';
   private $bname = 'Unknown';
-  private $version = "";
+  private $version = '';
   private $platform = 'Unknown';
   private $pattern = '';
   private $sname = '';
-*/
-  /*public function BrowserInfo() { 
-    readPlatform();
-    readName();
-    readVersion();
-  }
 
+  public function BrowserInfo() { 
+    $this->uagent = $_SERVER['HTTP_USER_AGENT'];
+    $this->readPlatform();
+    $this->readName();
+    $this->readVersion();
+  }
+  
   private function readPlatform() {
-    if (preg_match('/linux/i', $uagent)) {
-      $platform = 'linux';
-    }elseif (preg_match('/macintosh|mac os x/i', $uagent)) {
-      $platform = 'mac';
-    }elseif (preg_match('/windows|win32/i', $uagent)) {
-      $platform = 'windows';
+    if (preg_match('/Android/i', $this->uagent)) {
+      $this->platform = 'Android';  
+    }elseif(preg_match('/linux/i', $this->uagent)) {
+      $this->platform = 'Linux';
+    }elseif (preg_match('/macintosh|mac os x/i', $this->uagent)) {
+      $this->platform = 'Mac';
+    }elseif (preg_match('/windows|win32/i', $this->uagent)) {
+      $this->platform = 'Windows';
     }
   }
 
   private function readName() {
+    $uagent = $this->uagent;
     if(preg_match('/MSIE/i',$uagent) && !preg_match('/Opera/i',$uagent)){
-      $bname = 'Internet Explorer';
-      $sname = "MSIE";
+      $this->bname = 'Internet Explorer';
+      $this->sname = "MSIE";
     }elseif(preg_match('/Firefox/i',$uagent)){
-      $bname = 'Mozilla Firefox';
-      $sname = "Firefox";
+      $this->bname = 'Mozilla Firefox';
+      $this->sname = "Firefox";
     }elseif(preg_match('/OPR/i',$uagent)){
-      $bname = 'Opera';
-      $sname = "Opera";
+      $this->bname = 'Opera';
+      $this->sname = "Opera";
+    }elseif(preg_match('/SamsungBrowser/i',$u_agent)){
+      $this->bname = 'Samsung Browser';
+      $this->sname = 'SamsungBrowser';
     }elseif(preg_match('/Chrome/i',$uagent) && !preg_match('/Edge/i',$uagent)){
-      $bname = 'Google Chrome';
-      $sname = "Chrome";
+      $this->bname = 'Google Chrome';
+      $this->sname = 'Chrome';
     }elseif(preg_match('/Safari/i',$uagent) && !preg_match('/Edge/i',$uagent)){
-      $bname = 'Apple Safari';
-      $sname = "Safari";
+      $this->bname = 'Apple Safari';
+      $this->sname = "Safari";
     }elseif(preg_match('/Netscape/i',$uagent)){
-      $bname = 'Netscape';
-      $sname = "Netscape";
+      $this->bname = 'Netscape';
+      $this->sname = "Netscape";
     }elseif(preg_match('/Edge/i',$uagent)){
-      $bname = 'Edge';
-      $sname = "Edge";
+      $this->bname = 'Edge';
+      $this->sname = "Edge";
     }elseif(preg_match('/Trident/i',$uagent)){
-      $bname = 'Internet Explorer';
-      $sname = "MSIE";
+      $this->bname = 'Internet Explorer';
+      $this->sname = "MSIE";
+    }elseif(preg_match('/Lynx/i',$u_agent)){
+      $this->bname = 'Lynx';
+      $this->sname = "Lynx";
+    }elseif(preg_match('/w3m/i',$u_agent)){
+      $this->bname = 'W3M';
+      $this->sname = "w3m";
+    }
+    
+    if (strpos($u_agent, 'curl') !== false) {
+      $this->bname = 'Curl';
+      $this->sname = 'Curl';
     }
   }
 
   private function readVersion(){
-    $known = array('Version', $sname, 'other');
-    $pattern = '#(?<browser>' . join('|', $known) .
-  ')[/ ]+(?<version>[0-9.|a-zA-Z.]*)#';
-    if (!preg_match_all($pattern, $uagent, $matches)) {
+    $known = array('Version', $this->sname, 'other');
+    $pattern = '#(?<browser>' . join('|', $known) . ')[/ ]+(?<version>[0-9.|a-zA-Z.]*)#';
+    if (!preg_match_all($pattern, $this->uagent, $matches)) {
       // we have no matching number just continue
     }
     // see how many we have
     $i = count($matches['browser']);
+    
     if ($i != 1) {
       //we will have two since we are not using 'other' argument yet
       //see if version is before or after the name
-      if (strripos($uagent,"Version") < strripos($uagent,$sname)){
-          $version= $matches['version'][0];
+      if (strripos($this->uagent,"Version") < strripos($this->uagent,$this->sname)){
+        $this->version= $matches['version'][0];
       }else {
-          $version= $matches['version'][1];
+        $this->version= $matches['version'][1];
       }
     }else {
-      $version= $matches['version'][0];
+      $this->version= $matches['version'][0];
     }
 
     // check if we have a number
-    if ($version==null || $version=="") {$version="?";}
-  }
-*/
-/*
-  public function getUserAgent{
-    return $uagent;
+    if ($this->version==null || $this->version=="") {
+      $this->version='?';
+    }
   }
 
-  public function getName{
-    return $bname;
+  public function getUserAgent(){
+    return $this->uagent;
   }
 
-  public function getVersion{
-    return $version;
+  public function getName(){
+    return $this->bname;
   }
 
-  public function getPlatform{
-    return $platform;
+  public function getVersion(){
+    return $this->version;
   }
 
-  public function getPattern{
-    return $pattern;
+  public function getPlatform(){
+    return $this->platform;
+  }
+
+  public function getPattern(){
+    return $this->pattern;
   }
 } 
-*/
 ?> 
--- src/index.php
+++ src/index.php
@@ -17,120 +17,15 @@
  </nav>
  <p />
  <div class="container">
- <?php 
-
-function getBrowser() { 
-  $u_agent = $_SERVER['HTTP_USER_AGENT'];
-  $bname = 'Unknown';
-  $platform = 'Unknown';
-  $version= "";
-
-  //First get the platform?
-  if (preg_match('/Android/i', $u_agent)) {
-    $platform = 'Android';  
-  }elseif(preg_match('/linux/i', $u_agent)) {
-    $platform = 'Linux';
-  }elseif (preg_match('/macintosh|mac os x/i', $u_agent)) {
-    $platform = 'Mac';
-  }elseif (preg_match('/windows|win32/i', $u_agent)) {
-    $platform = 'Windows';
-  }
-
-  // Next get the name of the useragent yes seperately and for good reason
-  if(preg_match('/MSIE/i',$u_agent) && !preg_match('/Opera/i',$u_agent)){
-    $bname = 'Internet Explorer';
-    $ub = "MSIE";
-  }elseif(preg_match('/Firefox/i',$u_agent)){
-    $bname = 'Mozilla Firefox';
-    $ub = "Firefox";
-  }elseif(preg_match('/OPR/i',$u_agent)){
-    $bname = 'Opera';
-    $ub = "Opera";
-  }elseif(preg_match('/SamsungBrowser/i',$u_agent)){
-    $bname = 'Samsung Browser';
-    $ub = "SamsungBrowser";
-  }elseif(preg_match('/Chrome/i',$u_agent) && !preg_match('/Edge/i',$u_agent)){
-    $bname = 'Google Chrome';
-    $ub = "Chrome";
-  }elseif(preg_match('/Safari/i',$u_agent) && !preg_match('/Edge/i',$u_agent)){
-    $bname = 'Apple Safari';
-    $ub = "Safari";
-  }elseif(preg_match('/Netscape/i',$u_agent)){
-    $bname = 'Netscape';
-    $ub = "Netscape";
-  }elseif(preg_match('/Edge/i',$u_agent)){
-    $bname = 'Edge';
-    $ub = "Edge";
-  }elseif(preg_match('/Trident/i',$u_agent)){
-    $bname = 'Internet Explorer';
-    $ub = "MSIE";
-  }elseif(preg_match('/Lynx/i',$u_agent)){
-    $bname = 'Lynx';
-    $ub = "Lynx";
-  }elseif(preg_match('/w3m/i',$u_agent)){
-    $bname = 'W3M';
-    $ub = "w3m";
-  }
-
-  // finally get the correct version number
-  $known = array('Version', $ub, 'other');
-  $pattern = '#(?<browser>' . join('|', $known) .
-')[/ ]+(?<version>[0-9.|a-zA-Z.]*)#';
-  if (!preg_match_all($pattern, $u_agent, $matches)) {
-    // we have no matching number just continue
-  }
-  // see how many we have
-  $i = count($matches['browser']);
-  if ($i != 1) {
-    //we will have two since we are not using 'other' argument yet
-    //see if version is before or after the name
-    if (strripos($u_agent,"Version") < strripos($u_agent,$ub)){
-        $version= $matches['version'][0];
-    }else {
-        $version= $matches['version'][1];
-    }
-  }else {
-    $version= $matches['version'][0];
-  }
-
-  // check if we have a number
-  if ($version==null || $version=="") {$version="?";}
-
-  // handle curl
-  if (strpos($u_agent, 'curl') !== false) {
-    $bname = 'Curl';
-    $u_name = 'Curl';
-  }
-
-  return array(
-    'userAgent' => $u_agent,
-    'name'      => $bname,
-    'version'   => $version,
-    'platform'  => $platform,
-    'pattern'   => $pattern
-  );
-} 
-
-$ua=getBrowser();
-$yourbrowser= "Your browser: " . $ua['name'] . " v" . $ua['version'] . " on " .$ua['platform'] . " reports: <br ><i>" . $ua['userAgent'] . "</i>";
-print_r($yourbrowser);
-
-echo "<br><br>Time: " . date("H:i:s") . " GMT<br>";
-echo "Date: " . date("d/m/Y");
-
-  //require_once('BrowserInfo.php');
-
-  //$b = new BrowserInfo;
-
-  //echo $b->getName();
-/*
-  printf("UserAgent: %s\nName: %s\nVersion: %s\nPlatform: %s\n", 
-    $browser->getUserAgent(),
-    $browser->getName(),
-    $browser->getVersion(),
-    $browser->getPlatform());
-*/
- ?>  
+ <?php
+  include_once('BrowserInfo.php');
+  $b = new BrowserInfo();
+  $yourbrowser= "<div id='info'>Your browser: " . $b->getName() . " v" . $b->getVersion() . " on " . $b->getPlatform() . " reports: </div><i>" . $b->getUserAgent() . "</i>";
+  print_r($yourbrowser);
+  echo "<br><br>Time: " . date("H:i:s") . " GMT<br>";
+  echo "Date: " . date("d/m/Y");
+  echo '<br>PHP version: ' . phpversion();
+?>  
 </div>
 </body>
 </html>
