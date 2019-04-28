164b85df195e75d6818dde073d54198f7218a292
Costa <costa365@gmail.com>
2019-04-28 19:24:20 +0300

    Fixed some typo bugs

--- src/browserInfo.php
+++ src/browserInfo.php
@@ -38,7 +38,7 @@ class browserInfo {
     }elseif(preg_match('/OPR/i',$uagent)){
       $this->bname = 'Opera';
       $this->sname = "Opera";
-    }elseif(preg_match('/SamsungBrowser/i',$u_agent)){
+    }elseif(preg_match('/SamsungBrowser/i',$uagent)){
       $this->bname = 'Samsung Browser';
       $this->sname = 'SamsungBrowser';
     }elseif(preg_match('/Chrome/i',$uagent) && !preg_match('/Edge/i',$uagent)){
@@ -56,15 +56,15 @@ class browserInfo {
     }elseif(preg_match('/Trident/i',$uagent)){
       $this->bname = 'Internet Explorer';
       $this->sname = "MSIE";
-    }elseif(preg_match('/Lynx/i',$u_agent)){
+    }elseif(preg_match('/Lynx/i',$uagent)){
       $this->bname = 'Lynx';
       $this->sname = "Lynx";
-    }elseif(preg_match('/w3m/i',$u_agent)){
+    }elseif(preg_match('/w3m/i',$uagent)){
       $this->bname = 'W3M';
       $this->sname = "w3m";
     }
     
-    if (strpos($u_agent, 'curl') !== false) {
+    if (strpos($uagent, 'curl') !== false) {
       $this->bname = 'Curl';
       $this->sname = 'Curl';
     }
@@ -73,15 +73,9 @@ class browserInfo {
   private function readVersion(){
     $known = array('Version', $this->sname, 'other');
     $pattern = '#(?<browser>' . join('|', $known) . ')[/ ]+(?<version>[0-9.|a-zA-Z.]*)#';
-    if (!preg_match_all($pattern, $this->uagent, $matches)) {
-      // we have no matching number just continue
-    }
-    // see how many we have
     $i = count($matches['browser']);
     
     if ($i != 1) {
-      //we will have two since we are not using 'other' argument yet
-      //see if version is before or after the name
       if (strripos($this->uagent,"Version") < strripos($this->uagent,$this->sname)){
         $this->version= $matches['version'][0];
       }else {
@@ -91,7 +85,6 @@ class browserInfo {
       $this->version= $matches['version'][0];
     }
 
-    // check if we have a number
     if ($this->version==null || $this->version=="") {
       $this->version='?';
     }
--- src/index.php
+++ src/index.php
@@ -16,8 +16,7 @@
 <div class="col c4"><h3>CPU Info</h3>View benchmarks and information about x86/x64 CPUs<br><a href="http://cpubenchmark.co.nf" class="btn btn-sm btn-a">Check it out!</a></div>
 <div class="col c4"><h3>Blood Glucose Converter</h3>Convert between mg/dL and mmol/L<br><a href="http://bloodglucoseconverter.co.nf/" class="btn btn-sm btn-a">Check it out!</a></div>    
 <div class="col c4"><h3>Phone Info</h3>View information about mobile phones<br><a href="http://fonoapi.co.nf/" class="btn btn-sm btn-a">Check it out!</a></div>
-<div class="col c4"><h3>vPoker</h3>Android Video Poker Game on Google Play<br><a href="https://play.google.com/store/apps/details?id=com.vPoker" class="btn btn-sm btn-a">Check it out!</a></div>
-<div class="col c4"><h3>vPoker</h3>Android Video Poker Game Home Page and link to SlideMe<br><a href="http://vpoker.co.nf/" class="btn btn-sm btn-a">Check it out!</a></div>
+<div class="col c4"><h3>vPoker</h3>Android Video Poker Game<br><a href="http://vpoker.co.nf" class="btn btn-sm btn-a">Check it out!</a></div>
 </div>
 </div>
   
