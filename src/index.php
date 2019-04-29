83e95824da1e77a4a3a14fd204ed07604f519609
Costa <costa365@gmail.com>
2019-04-29 19:32:45 +0300

    Improved index page

--- src/index.php
+++ src/index.php
@@ -9,17 +9,16 @@
  <?php
  include_once('header.php');
  ?>
-<div class="container">
-<div class="hero">
-</div>
-<div class="row">
-<div class="col c4"><h3>CPU Info</h3>View benchmarks and information about x86/x64 CPUs<br><a href="http://cpubenchmark.co.nf" class="btn btn-sm btn-a">Check it out!</a></div>
-<div class="col c4"><h3>Blood Glucose Converter</h3>Convert between mg/dL and mmol/L<br><a href="http://bloodglucoseconverter.co.nf/" class="btn btn-sm btn-a">Check it out!</a></div>    
-<div class="col c4"><h3>Phone Info</h3>View information about mobile phones<br><a href="http://fonoapi.co.nf/" class="btn btn-sm btn-a">Check it out!</a></div>
-<div class="col c4"><h3>vPoker</h3>Android Video Poker Game<br><a href="http://vpoker.co.nf" class="btn btn-sm btn-a">Check it out!</a></div>
-</div>
-</div>
-  
-</body>
+ <div class="container">
+ <div class="hero">
+ </div>
+ <div class="row">
+ <div class="col c4"><h3>CPU Info</h3>View benchmarks and information about x86/x64 CPUs<br><a href="http://cpubenchmark.co.nf" class="btn btn-sm btn-a">Check it out!</a></div>
+ <div class="col c4"><h3>Blood Glucose Converter</h3>Convert between mg/dL and mmol/L blood glucose units<br><a href="http://bloodglucoseconverter.co.nf/" class="btn btn-sm btn-a">Check it out!</a></div>    
+ <div class="col c4"><h3>Phone Info</h3>View information about mobile phones, obtained via Fono Api<br><a href="http://fonoapi.co.nf/" class="btn btn-sm btn-a">Check it out!</a></div>
+ <div class="col c4"><h3>vPoker</h3>Android Video Poker game available on Google Play and SlideMe<br><a href="http://vpoker.co.nf" class="btn btn-sm btn-a">Check it out!</a></div>
+ </div>
+ </div>
+ </body>
 </html>
 
