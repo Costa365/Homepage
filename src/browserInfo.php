01b78fd0619d8dbbdea92b62df0ec5a68f51a3b0
Costa <costa365@gmail.com>
2019-04-30 21:06:14 +0300

    Improved browser detection

--- src/browserInfo.php
+++ src/browserInfo.php
@@ -26,57 +26,76 @@ class browserInfo {
       $this->platform = $this->readMacVersion();      
     }elseif (preg_match('/iPad/i', $this->uagent)) {
       $this->platform = $this->readMacVersion();
+    }elseif (preg_match('/Windows Phone/i', $this->uagent)) {
+      $this->platform = 'Windows Phone';
     }elseif (preg_match('/windows|win32/i', $this->uagent)) {
       $this->platform = $this->readWindowsVersion();
     }
   }
 
   private function readWindowsVersion() {
+    $winVer = '';
     if (strpos($this->uagent, 'Windows 95') !== false) {
-      return 'Windows 95';
+      $winVer = 'Windows 95';
     }elseif (strpos($this->uagent, 'Win95') !== false) {
-      return 'Windows 95';
+      $winVer = 'Windows 95';
     }elseif (strpos($this->uagent, 'Windows_95') !== false) {
-      return 'Windows 95';
+      $winVer = 'Windows 95';
     }elseif (strpos($this->uagent, 'Windows 98') !== false) {
-      return 'Windows 98';
+      $winVer = 'Windows 98';
     }elseif (strpos($this->uagent, 'Win98') !== false) {
-      return 'Windows 98';
+      $winVer = 'Windows 98';
     }elseif (strpos($this->uagent, 'Windows NT 5.0') !== false) {
-      return 'Windows 2000';
+      $winVer = 'Windows 2000';
     }elseif (strpos($this->uagent, 'Windows 2000') !== false) {
-      return 'Windows 2000';
+      $winVer = 'Windows 2000';
     }elseif (strpos($this->uagent, 'Windows NT 5.1') !== false) {
-      return 'Windows XP';
+      $winVer = 'Windows XP';
     }elseif (strpos($this->uagent, 'Windows XP') !== false) {
-      return 'Windows XP';
+      $winVer = 'Windows XP';
     }elseif (strpos($this->uagent, 'Windows NT 5.2') !== false) {
-      return 'Windows Server 2003';
+      $winVer = 'Windows Server 2003';
     }elseif (strpos($this->uagent, 'Windows NT 6.0') !== false) {
-      return 'Windows Vista';
+      $winVer = 'Windows Vista';
     }elseif (strpos($this->uagent, 'Windows NT 6.1') !== false) {
-      return 'Windows 7';
+      $winVer = 'Windows 7';
     }elseif (strpos($this->uagent, 'Windows NT 6.2') !== false) {
-      return 'Windows 8';
+      $winVer = 'Windows 8';
     }elseif (strpos($this->uagent, 'Windows NT 10.0') !== false) {
-      return 'Windows 10';
+      $winVer = 'Windows 10';
     }elseif (strpos($this->uagent, 'Windows ME') !== false) {
-      return 'Windows ME';
+      $winVer = 'Windows ME';
     }elseif (strpos($this->uagent, 'Windows NT 4.0') !== false) {
-      return 'Windows NT 4.0';
+      $winVer = 'Windows NT 4.0';
     }elseif (strpos($this->uagent, 'WinNT4.0') !== false) {
-      return 'Windows NT 4.0';
+      $winVer = 'Windows NT 4.0';
     }elseif (strpos($this->uagent, 'WinNT') !== false) {
-      return 'Windows NT 4.0';
+      $winVer = 'Windows NT 4.0';
     }elseif (strpos($this->uagent, 'Windows NT') !== false) {
-      return 'Windows NT 4.0';
+      $winVer = 'Windows NT 4.0';
     }elseif (strpos($this->uagent, 'WinNT') !== false) {
-      return 'Windows NT 4.0';
+      $winVer = 'Windows NT 4.0';
     }elseif (strpos($this->uagent, 'WinNT') !== false) {
-      return 'Windows NT 4.0';
+      $winVer = 'Windows NT 4.0';
     }elseif (strpos($this->uagent, 'WinNT') !== false) {
-      return 'Windows NT 4.0';
+      $winVer = 'Windows NT 4.0';
     }
+
+    return $winVer . ' (' . $this->cpuBitLength() . ')';
+  }
+
+  private function cpuBitLength() {
+    if (strpos($this->uagent,"WOW64") == true ||
+      strpos($this->uagent,"Win64") == true ||
+      strpos($this->uagent,"x86_64") == true ||
+      strpos($this->uagent,"x86-64") == true ||
+      strpos($this->uagent,"x64;") == true ||
+      strpos($this->uagent,"amd64") == true ||
+      strpos($this->uagent,"AMD64") == true ||
+      strpos($this->uagent,"amd64") == true) {
+      return "64-bit";
+    }
+    return "32-bit";
   }
 
   private function readAndroidVersion() {
@@ -160,6 +179,9 @@ class browserInfo {
     }elseif(preg_match('/w3m/i',$uagent)){
       $this->bname = 'W3M';
       $this->sname = "w3m";
+    }elseif(preg_match('/Opera/i',$uagent)){
+      $this->bname = 'Opera';
+      $this->sname = "Opera";
     }
     
     if (strpos($uagent, 'curl') !== false) {
