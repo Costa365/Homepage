3ed84145ea24c7910ece684ca0d194b868610f2e
Costa <costa365@gmail.com>
2019-05-04 16:28:35 +0300

    Added icon to header

--- src/header.php
+++ src/header.php
@@ -1,6 +1,6 @@
  <nav class="nav" tabindex="-1" onclick="this.focus()">
   <div class="container">
-   <a class="pagename current" href="/">Costa365</a>
+   <a class="pagename current" href="/"><img style="margin-bottom: -9; margin-right: 3" src="images/globe.png" height="32" width="32">Costa365</a>
    <a href="client.php">Client Information</a> 
    <a href="server.php">Server Information</a>
   </div>
