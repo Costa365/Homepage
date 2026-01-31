56ba21abc46798c578cc718778156221276fb891
Costa <costa365@gmail.com>
2026-01-31 14:39:53 +0200

    Updated style of client and server pages

--- src/client.php
+++ src/client.php
@@ -4,6 +4,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Costa365 - Client</title>
   <link href="entireframework.min.css" rel="stylesheet" type="text/css">
+  <link href="css/styles.css" rel="stylesheet" type="text/css">
  </head>
  <body>
  <?php
