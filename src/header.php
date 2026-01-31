09a432c027646be4e117863edcd3e4ed484b6c2c
Costa <costa365@gmail.com>
2026-01-31 14:54:49 +0200

    Updated nav bar style

--- src/header.php
+++ src/header.php
@@ -1,13 +1,15 @@
 <nav class="nav">
-    <a class="pagename current" href="/"><img src="images/logo.png" alt="Logo" height="34"></a>
-    <button class="burger-menu" aria-label="Toggle menu" onclick="document.querySelector('.nav').classList.toggle('nav-open')">
-        <span></span>
-        <span></span>
-        <span></span>
-    </button>
-    <div class="nav-links">
-        <a href="client.php">Client Information</a>
-        <a href="server.php">Server Information</a>
+    <div class="nav-container">
+        <a class="pagename current" href="/"><img src="images/logo.png" alt="Logo" height="34"></a>
+        <button class="burger-menu" aria-label="Toggle menu" onclick="document.querySelector('.nav').classList.toggle('nav-open')">
+            <span></span>
+            <span></span>
+            <span></span>
+        </button>
+        <div class="nav-links">
+            <a href="client.php">Client Information</a>
+            <a href="server.php">Server Information</a>
+        </div>
     </div>
 </nav>
 
